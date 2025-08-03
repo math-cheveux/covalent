dc1953e5566ad5e4d60eb25eb09ad836a01cdf17
math-cheveux <math.cheveux@gmail.com>
2025-08-03 21:32:59 +0200

    test(render): add "should have default behavior" test

--- packages/render/test/browser.test.ts
+++ packages/render/test/browser.test.ts
@@ -1,3 +1,4 @@
+import { Bridge } from "@covalent/common";
 import { Bridges } from "../src";
 import { ExampleProxy, LogProxy } from "./test-interfaces";
 
@@ -24,5 +25,34 @@ describe("browser-side without electron", () => {
     await expect(exampleProxy.getConfiguration()).resolves.not.toThrow();
     await expect(exampleProxy.calculate({ x: 0 })).resolves.not.toThrow();
     await expect(exampleProxy.watch({ period: 100 })).resolves.not.toThrow();
+  });
+
+  test("should have default behavior", async () => {
+    interface TestBridge {
+      testSend: Bridge.Send<string>;
+      testInvoke: Bridge.Invoke<string, string>;
+      testCallback: Bridge.Callback<string, string>;
+    }
+
+    const defaultBridge: TestBridge = {
+      testSend: jest.fn(),
+      testInvoke: jest.fn(),
+      testCallback: jest.fn(),
+    };
+    const sendSpy = jest.spyOn(defaultBridge, "testSend");
+    const invokeSpy = jest.spyOn(defaultBridge, "testInvoke");
+    const callbackSpy = jest.spyOn(defaultBridge, "testCallback");
+
+    const bridge = Bridges.bind<TestBridge>("test", defaultBridge);
+
+    expect(sendSpy).not.toHaveBeenCalled();
+    bridge.testSend("test");
+    expect(sendSpy).toHaveBeenCalled();
+    expect(invokeSpy).not.toHaveBeenCalled();
+    bridge.testInvoke("test");
+    expect(invokeSpy).toHaveBeenCalled();
+    expect(callbackSpy).not.toHaveBeenCalled();
+    bridge.testCallback(() => {}, "test");
+    expect(callbackSpy).toHaveBeenCalled();
   });
 });
